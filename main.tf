fa86bf00715ff199ffdf22caf745c20f041f2bb0
Ryan Faircloth <ryan@dss-i.com>
2022-10-11 17:17:56 -0400

    feat: external provider

--- main.tf
+++ main.tf
@@ -1,43 +1,84 @@
 
-provider "kubernetes" {
-  host                   = var.eks_endpoint
-  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
 
-  exec {
-    api_version = "client.authentication.k8s.io/v1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
-  }
-}
+module "karpenter_irsa" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
+  role_name                          = "${var.uniqueName}_karpenter_controller"
+  attach_karpenter_controller_policy = true
 
-provider "kubectl" {
-  apply_retry_count      = 10
-  load_config_file       = false
+  karpenter_tag_key               = "karpenter.sh/discovery/${var.uniqueName}"
+  karpenter_controller_cluster_id = var.eks_cluster_id
+  karpenter_controller_node_iam_role_arns = [
+    var.eks_karpenter_iam_role_arn
+  ]
 
-  host                   = var.eks_endpoint
-  cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
-  }
-
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = var.eks_endpoint
-    cluster_ca_certificate = base64decode(var.eks_cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1"
-      command     = "aws"
-      # This requires the awscli to be installed locally where Terraform is executed
-      args = ["eks", "get-token", "--cluster-name", var.eks_cluster_id]
+  oidc_providers = {
+    main = {
+      provider_arn               = var.eks_oidc_provider_arn
+      namespace_service_accounts = ["karpenter:karpenter"]
     }
   }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
 }
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${var.uniqueName}"
+  role = var.eks_karpenter_iam_role_name
+}
+
+
+
+resource "kubernetes_namespace" "karpenter" {
+
+  metadata {
+    name = "karpenter"
+  }
+}
+
+resource "helm_release" "karpenter" {
+  namespace        = kubernetes_namespace.karpenter.metadata[0].name
+  create_namespace = false
+
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.15.0"
+
+  values = [<<EOF
+topologySpreadConstraints:
+  - maxSkew: 1
+    topologyKey: topology.kubernetes.io/zone
+    whenUnsatisfiable: DoNotSchedule
+EOF 
+  ]
+  set {
+    name  = "replicas"
+    value = 2
+  }
+
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter_irsa.iam_role_arn
+  }
+
+  set {
+    name  = "clusterName"
+    value = var.eks_cluster_id
+  }
+
+  set {
+    name  = "clusterEndpoint"
+    value = var.eks_endpoint
+  }
+
+  set {
+    name  = "aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.karpenter.name
+  }
+}
+
