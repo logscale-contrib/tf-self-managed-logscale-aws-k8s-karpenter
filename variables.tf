caf42babb3065e20558a3465e42268a9497f9800
Ryan Faircloth <ryan@dss-i.com>
2022-10-11 17:40:45 -0400

    fix: remove unused vars

--- variables.tf
+++ variables.tf
@@ -3,6 +3,15 @@ variable "uniqueName" {
   description = "(optional) describe your variable"
 }
 
+variable "eks_cluster_id" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+variable "eks_endpoint" {
+  type        = string
+  description = "(optional) describe your variable"
+}
+
 variable "eks_oidc_provider_arn" {
   type        = string
   description = "(optional) describe your variable"
